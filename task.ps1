2c3aff906afe1ae3fd512fc21d1289b9c6b99938
Владислав Устьянцев <vladisloveust@gmail.com>
2026-04-22 23:06:40 +0300

    Solution1

--- task.ps1
+++ task.ps1
@@ -1,1 +1,15 @@
-# Write your code here
+$results = New-Object System.Collections.Generic.List[PSObject]
+
+$files = Get-ChildItem -Path ".\data" -File
+
+foreach ($file in $files) {
+    $data_massive = Get-Content -Raw -Path $file.FullName | ConvertFrom-Json
+
+    foreach ($data in $data_massive) {
+        if ($data.Name -eq "Standard_B2pts_v2") {
+            $results.Add($file.Name)
+        }
+    }
+}
+
+$results | ConvertTo-Json -Depth 5 | Out-File -FilePath ".\result.json" -Encoding UTF8
